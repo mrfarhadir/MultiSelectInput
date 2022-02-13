4445f47be4e004d8cfbd50c6f76e26daa6477737
Farhad Mehryari <info@mrfarhad.ir>
2022-02-14 00:26:09 +0330

    Import MultiSelect in App component with adding some styles for App (demo purpose)

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-
+import './App.css'
+import {MultiSelect} from "./components/MultiSelect";
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+	return (
+		<div className="App">
+			<MultiSelect />
+		</div>
+	);
 }
 
 export default App;
