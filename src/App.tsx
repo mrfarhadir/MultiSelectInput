cb5a37cd0f5b8918a6f3602bb4aabc129e00d8da
Farhad Mehryari <info@mrfarhad.ir>
2022-02-14 00:21:34 +0330

    create initial component for multi select

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 
 function App() {
   return (
     <div className="App">
-     
+
     </div>
   );
 }
---
+++ src/components/MultiSelect/index.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background: var(--light);
+  height: 48px;
+  border-radius: 12px;
+`
+
+
+export const MultiSelect = () => {
+	return (
+		<Wrapper>
+			<input />
+		</Wrapper>
+	)
+}
